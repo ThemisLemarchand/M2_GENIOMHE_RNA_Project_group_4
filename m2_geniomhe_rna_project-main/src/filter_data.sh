82d0f4b198a259c745ac1ab1d5dd40368b152d12
ThemisLemarchand <156213749+ThemisLemarchand@users.noreply.github.com>
2024-01-11 18:43:14 +0100

    remove identifier

--- m2_geniomhe_rna_project-main/src/filter_data.sh
+++
@@ -1,8 +0,0 @@
-#rm output.csv
-for f in $(ls data/angles/TestSet); 
-    do 
-        cp data/angles/TestSet/${f} temp
-       # cat temp
-        sed -i '1d' temp        
-        cut -d "," -f 8,20 temp >> output_test.csv;
-done;
